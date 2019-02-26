68fa6be6505ae48f1aa3fdfc877ecc3fbe611057
Michael Walker <mike@barrucadu.co.uk>
2019-02-26 18:06:17 +0000

    Use a versioned S3 bucket with lifecycle for backups
    My current backup script is a script I run manually on the last Sunday
    of every month, which rsyncs a bunch of stuff to innsmouth, and then a
    second script I run on innsmouth to push that into Glacier.  For
    various reasons, it's not really robust enough to run unsupervised, so
    I can't automate it.
    
    To allow automated backups, I've rewritten my script to use
    [duplicity][].  This is great, it can be automated, and it allows for
    incremental backups (and so more frequent backups).  But it needs to
    be able to read metadata stored on the remote, so it can't use Glacier
    as primary storage.
    
    S3 is far more expensive than Glacier, so to trade-off between ease of
    access and price, after 60 days backups move into "infrequently
    access" storage, and then after a further 120 days into Glacier.  The
    backup script only cares about the most recent full backup (and
    incremental backups since then), so as long as a full backup is taken
    at least once every 180 days, this works out just fine.
    
    [duplicity]: http://duplicity.nongnu.org/

--- projects/backups/main.tf
+++ projects/backups/main.tf
@@ -1,5 +1,25 @@
-resource "aws_glacier_vault" "backup" {
-  name = "backup"
+resource "aws_s3_bucket" "backup" {
+  bucket = "barrucadu-backups"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    id      = "archive"
+    enabled = true
+
+    transition {
+      days          = 60
+      storage_class = "STANDARD_IA"
+    }
+
+    transition {
+      days          = 120
+      storage_class = "GLACIER"
+    }
+  }
 }
 
 /* ************************************************************************* */
@@ -9,31 +29,48 @@ variable "group_backup_user_names" {
 }
 
 module "group_backup" {
-  source                  = "../../modules/group_user"
-  group_name              = "backup"
-  group_user_names        = ["${var.group_backup_user_names}"]
-  group_policy_arns       = ["${aws_iam_policy.backup.arn}"]
+  source           = "../../modules/group_user"
+  group_name       = "backup"
+  group_user_names = ["${var.group_backup_user_names}"]
+
   group_policy_arns_count = 1
+  group_policy_arns       = [
+    "${aws_iam_policy.tool_duplicity.arn}",
+  ]
 }
 
-resource "aws_iam_policy" "backup" {
+resource "aws_iam_policy" "tool_duplicity" {
   policy = <<EOF
 {
-  "Version":"2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Resource": "${aws_glacier_vault.backup.arn}",
-      "Action": [
-        "glacier:UploadArchive",
-        "glacier:InitiateMultipartUpload",
-        "glacier:UploadMultipartPart",
-        "glacier:ListParts",
-        "glacier:ListMultipartUploads",
-        "glacier:CompleteMultipartUpload"
-      ]
-    }
-  ]
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": [
+                "s3:ListAllMyBuckets",
+                "s3:GetBucketLocation"
+            ],
+            "Effect": "Allow",
+            "Resource": [
+                "arn:aws:s3:::*"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:ListBucket",
+                "s3:ListBucketMultipartUploads",
+                "s3:ListMultipartUploadParts",
+                "s3:AbortMultipartUpload",
+                "s3:PutObject",
+                "s3:GetObject",
+                "s3:DeleteObject"
+             ],
+            "Resource": [
+                "${aws_s3_bucket.backup.arn}",
+                "${aws_s3_bucket.backup.arn}/*"
+            ]
+        }
+    ]
 }
 EOF
 }
--- projects/backups/provider.tf
+++ projects/backups/provider.tf
@@ -8,6 +8,6 @@ terraform {
 }
 
 provider "aws" {
-  region  = "eu-west-2"
+  region  = "eu-west-1"
   version = "~> 1.25"
 }
