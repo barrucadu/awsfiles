d73744f2749addc8f2f2b0088b6666927013051f
Michael Walker <mike@barrucadu.co.uk>
2020-09-14 17:46:54 +0100

    Create a group for octodns

--- terraform/projects/dns/main.tf
+++ terraform/projects/dns/main.tf
@@ -8,6 +8,41 @@ locals {
 
 /* ************************************************************************* */
 
+variable "group_dns_user_names" {
+  type = list(string)
+}
+
+module "group_dns" {
+  source           = "../../modules/group_user"
+  group_name       = "dns"
+  group_user_names = var.group_dns_user_names
+
+  group_policy_arns = [
+    aws_iam_policy.tool_octodns.arn,
+  ]
+}
+
+resource "aws_iam_policy" "tool_octodns" {
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": [
+                "route53:*"
+            ],
+            "Effect": "Allow",
+            "Resource": [
+                "*"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+/* ************************************************************************* */
+
 module "barrucadu_co_uk" {
   source = "../../modules/dns"
   domain = "barrucadu.co.uk"
