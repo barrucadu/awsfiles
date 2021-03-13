54f53a40cd4560fb46d206d325d453a06fe76b7c
Michael Walker <mike@barrucadu.co.uk>
2021-03-13 16:39:47 +0000

    Add a key & IAM policy for Concourse secrets

---
+++ terraform/projects/concourse/main.tf
@@ -0,0 +1,75 @@
+variable "aws_account_id" {
+  type = string
+}
+
+variable "group_concourse_user_names" {
+  type = list(string)
+}
+
+module "group_concourse" {
+  source           = "../../modules/group_user"
+  group_name       = "concourse"
+  group_user_names = var.group_concourse_user_names
+
+  group_policy_arns = [
+    aws_iam_policy.tool_concourse.arn,
+  ]
+}
+
+resource "aws_kms_key" "concourse" {
+  description = "Key for Concourse secrets"
+}
+
+resource "aws_iam_policy" "tool_concourse" {
+  policy = data.aws_iam_policy_document.tool_concourse.json
+}
+
+data "aws_iam_policy_document" "tool_concourse" {
+  statement {
+    sid = "ssm"
+
+    actions = [
+      "ssm:GetParameter",
+      "ssm:GetParametersByPath",
+      "ssm:PutParameter",
+    ]
+
+    resources = [
+      # "arn:aws:ssm:::parameter/..." doesn't seem to work in this
+      # policy, but specifying the account ID explicitly does.
+      "arn:aws:ssm:eu-west-2:${var.aws_account_id}:parameter/concourse",
+      "arn:aws:ssm:eu-west-2:${var.aws_account_id}:parameter/concourse/*",
+    ]
+  }
+
+  statement {
+    sid = "usekms"
+
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:DescribeKey",
+    ]
+
+    resources = [
+      aws_kms_key.concourse.arn
+    ]
+  }
+
+  statement {
+    sid = "listkms"
+
+    actions = [
+      "kms:ListAliases",
+      "kms:ListKeys",
+    ]
+
+    resources = [
+      "*"
+    ]
+  }
+}
+
+output "key_arn" {
+  value = aws_kms_key.concourse.arn
+}
